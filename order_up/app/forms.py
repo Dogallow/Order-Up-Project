c55fb3fe20deb6f305f4cff959253fff8dc4c7bc
Donovan Galloway <genericsalesman6@gmail.com>
2022-11-02 15:25:56 -0700

    Logout

--- order_up/app/forms.py
+++ order_up/app/forms.py
@@ -6,4 +6,4 @@ from wtforms.validators import DataRequired, ValidationError
 class LoginForm(FlaskForm):
     employee_number = StringField("Employee number", validators = [DataRequired()])
     password = PasswordField("Password", validators = [DataRequired()])
-    submit = SubmitField("Logi")
+    submit = SubmitField("Login")
